f3bf6189fb16fe39703ade5e28194da5536d6b2e
LamiaMed <hl_medjahed@esi.dz>
2022-06-26 18:11:16 +0100

    operateur controller

--- app/controllers/operateur.controller.js
+++ app/controllers/operateur.controller.js
@@ -1,3 +1,36 @@
 const db = require("../models");
 const Operateur = db.operateur;
 
+const findOperateurByID = async(req, res) =>{
+    try{
+        const operateur = await Operateur.findOne({
+            where:{
+                idOperateur: +req.params.id,
+            },
+        });
+        res.status(200).send(operateur);
+    } catch(err){
+        res.status(500).send({
+            error: err.message || 
+            'Some error occured while retrieving the operator' + 
+            req.params.id,
+        });
+    }
+};
+
+// Retrieve all Tutorials from the database.
+exports.getAllOperateurs = (req, res) => {
+    var condition = 1 === 1;
+  
+    Operateur.findAll({ where: condition })
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Some error occurred while retrieving Operateurs."
+        });
+      });
+  };
+  
--- app/routes/trajet.routes.js
+++ app/routes/trajet.routes.js
@@ -1,7 +1,16 @@
 module.exports = app => {
     const trajet = require("../controllers/trajet.controller.js");
-    var router = require("express").Router();
+    var trajetRouter = require("express").Router();
     // Create a new Tutorial
-    router.post("/", trajet.createTrajet);
+    trajetRouter.post("/", trajet.createTrajet);
+    trajetRouter.get("/", trajet.getTrajetByOperateur);
+    trajetRouter.get("/", trajet.getTrajetByPatient);
+    trajetRouter.get("/", trajet.getTrajetDouteux);
+    trajetRouter.get("/", trajet.getTrajetValide);
+    trajetRouter.post("/", trajet.updateTrajetByID);
+    trajetRouter.get("/", trajet.getAllTrajets);
+    trajetRouter.get("/", trajet.findTrajetByID);
+
     app.use('/api/trajet', router);
+    
   };
